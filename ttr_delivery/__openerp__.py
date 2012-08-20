cbbe5937a632229bd530265fb01eba87fc06646e
Ronald Portier <ronald@therp.nl>
2012-08-20 15:04:28 +0200

    [ENH] Added technotrading customization for delivery.

---
+++ ttr_delivery/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'ttr_delivery',
+    'description': '''Technotrading Delivery Customizations''',
+    'application': True,
+    'version': '6.0.r003',
+    'author': 'Therp',
+    'category': 'ttr modules',
+    'website': 'http://technotrading.nl',
+    'email': 'info@technotrading.nl',
+    'depends': [
+        'base',
+        'delivery',
+    ],
+    'update_xml': [
+        'report/delivery_report.xml',
+    ],
+}
