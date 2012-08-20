7c08cbb6cfc2543da4ca6cc8a35b224e0c07d32c
Ronald Portier <ronald@therp.nl>
2012-08-20 16:47:03 +0200

    [NEW] Added technotrading purchase customizations.

---
+++ ttr_purchase/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'ttr_purchase',
+    'description': '''Technotrading Purchase Customizations''',
+    'application': True,
+    'version': '6.0.r004',
+    'author': 'Therp',
+    'category': 'ttr modules',
+    'website': 'http://technotrading.nl',
+    'email': 'info@technotrading.nl',
+    'depends': [
+        'base',
+        'purchase',
+    ],
+    'update_xml': [
+        'report/purchase_report.xml',
+    ],
+}
---
+++ ttr_purchase/report/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 20 aug. 2012
+
+@author: Ronald Portier, Therp
+
+rportier@therp.nl
+http://www.therp.nl
+
+'''
+import request_quotation
