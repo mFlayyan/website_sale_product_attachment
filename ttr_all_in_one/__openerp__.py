7b62a2fe0a74c3cc18afecb61b30dcf7ced8a6f2
Ronald Portier <ronald@therp.nl>
2012-08-22 18:26:56 +0200

    [ENH] Single module to trigger installation of all Technotrading customizations.

---
+++ ttr_all_in_one/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'ttr_all_in_one',
+    'description': '''The one Technotrading module to rule them all''',
+    'application': True,
+    'version': '6.0.r012',
+    'author': 'Therp',
+    'category': 'ttr modules',
+    'website': 'http://technotrading.nl',
+    'email': 'info@technotrading.nl',
+    'depends': [
+        'technotrading',
+        'technotrading_custom',
+        'ttr_account',
+        'ttr_delivery',
+        'ttr_purchase',
+        'ttr_sale',
+        'ttr_stock',
+    ],
+    'update_xml': [
+    ],
+}
