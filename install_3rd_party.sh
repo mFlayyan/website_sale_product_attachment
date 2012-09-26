3a269d32f14c74c6cd5a1e0fd7745c3d4e5f0d81
Ronald Portier <ronald@therp.nl>
2012-09-26 16:11:24 +0200

    [FIX] Name for report replacing standard report must not only have the same id, but also the same name.

--- install_3rd_party.sh
+++ install_3rd_party.sh
@@ -5,6 +5,7 @@ else
     cd "$HOME/3rd_party_modules_6.0"
     rm ./*
 fi
+ln -s $HOME/bzr/banking-addons/account_banking $HOME/3rd_party_modules_6.0/account_banking
 ln -s $HOME/bzr/banking-addons/account_banking_nl_clieop $HOME/3rd_party_modules_6.0/account_banking_nl_clieop
 ln -s $HOME/bzr/banking-addons/account_banking_nl_abnamro $HOME/3rd_party_modules_6.0/account_banking_nl_abnamro
 ln -s $HOME/bzr/banking-addons/account_banking_nl_ing $HOME/3rd_party_modules_6.0/account_banking_nl_ing
