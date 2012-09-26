4407d0b93b5572fe5507829f0fd2d715b4423eda
Ronald Portier <ronald@therp.nl>
2012-09-26 13:36:53 +0200

    [ENH] Utility script to fill 3rd_party_modules-6.0 with the right stuff

---
+++ install_3rd_party.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [ ! -d "$HOME/3rd_party_modules_6.0" ]; then
+    mkdir "$HOME/3rd_party_modules_6.0"
+else
+    rm "$HOME/3rd_party_modules_6.0/*"
+fi
+ln -s $HOME/bzr/banking-addons/account_banking_nl_clieop $HOME/3rd_party_modules_6.0/account_banking_nl_clieop
+ln -s $HOME/bzr/banking-addons/account_banking_nl_abnamro $HOME/3rd_party_modules_6.0/account_banking_nl_abnamro
+ln -s $HOME/bzr/banking-addons/account_banking_nl_ing $HOME/3rd_party_modules_6.0/account_banking_nl_ing
+ln -s $HOME/bzr/extra-6.0/base_external_referentials $HOME/3rd_party_modules_6.0/base_external_referentials
+ln -s $HOME/custom-addons/base_report_xlwt $HOME/3rd_party_modules_6.0/base_report_xlwt
+ln -s $HOME/bzr/extra-6.0/base_sale_multichannels $HOME/3rd_party_modules_6.0/base_sale_multichannels
+ln -s $HOME/bzr/magentoerpconnect-6.0/magentoerpconnect $HOME/3rd_party_modules_6.0/magentoerpconnect
+ln -s $HOME/bzr/extra-6.0/product_images_olbs $HOME/3rd_party_modules_6.0/product_images_olbs
+ln -s $HOME/bzr/extra-6.0/product_links  $HOME/3rd_party_modules_6.0/product_links 
+ln -s $HOME/bzr/extra-6.0/product_m2mcategories  $HOME/3rd_party_modules_6.0/product_m2mcategories 
+ln -s $HOME/bzr/therp-addons/trp_update_tax  $HOME/3rd_party_modules_6.0/trp_update_tax
+
+exit 0
