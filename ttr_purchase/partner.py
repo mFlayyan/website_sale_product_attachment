c6b81e0373eb15219c34fe7f8dea9868cd415d8b
Hans van Dijk <hans@therp.nl>
2013-10-01 13:46:25 +0200

    [IMP] leading comment modified

--- ttr_purchase/partner.py
+++ ttr_purchase/partner.py
@@ -1,24 +1,14 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
-#
-##############################################################################
+# -*- coding: UTF-8 -*-
+'''
+Created on 1 oct. 2013
 
+@author: Hans van Dijk, Therp
+
+hvandijk@therp.nl
+http://www.therp.nl
+
+Additional fields in favor of the purchase proposal
+'''
 from osv import fields, osv
 
 class res_partner(osv.osv):
