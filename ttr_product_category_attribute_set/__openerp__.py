ad307cde14ecbeb9ebc004d0ecc9cb6001c74353
gfcapalbo <giovanni@therp.nl>
2016-08-31 12:42:58 +0200

    changes to manifest

--- ttr_product_category_attribute_set/__openerp__.py
+++ ttr_product_category_attribute_set/__openerp__.py
@@ -2,23 +2,22 @@
 # © 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "ttr_product_category_attribute_set",
+    "name": "Technotrading attribute sets on Product Category",
     "version": "9.0.1.0.0",
     "author": "Therp BV",
     "license": "AGPL-3",
-    "category": "",
-    "summary": "",
+    "category": "Sales",
+    "description": """
+Extends Product category to have attribute sets for
+emulating the old magento attribute groups.
+""",
     "depends": [ 
         'sale',
     ],
     "data": [
         'data/data.xml',
         ],
-    #"post_init_hook": "post_init_hook",
     "auto_install": False,
     "installable": True,
     "application": False,
-    "external_dependencies": {
-        'python': [],
-    },
 }
