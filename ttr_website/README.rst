926b2bb9461b70e4803e54306ad97be2994a4f46
Holger Brunn <hbrunn@therp.nl>
2016-09-07 17:30:45 +0200

    [ADD] very basic multi website setup

---
+++ ttr_website/README.rst
@@ -0,0 +1,12 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+====================================
+Website customizations Technotrading
+====================================
+
+This modules configures Technotrading's websites
+
+* three websites with dummy addresses for now
+* add everything else you change here
---
+++ ttr_website/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Website customizations Technotrading",
+    "version": "9.0.1.0.0",
+    "author": "Therp BV",
+    "license": "AGPL-3",
+    "category": "Website",
+    "summary": "This module configures technotrading's websites",
+    "depends": [
+        'website',
+    ],
+    "data": [
+        "views/templates.xml",
+        "data/website.xml",
+    ],
+}
