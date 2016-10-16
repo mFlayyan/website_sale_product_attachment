99a5d2f51ca5250085c59a6809ea81b7196d8458
Mariam Flayyan <mflayyan@therp.nl>
2016-10-16 18:57:07 +0300

    add attachments to products

---
+++ product_attachments/models/product_attachment.py
@@ -0,0 +1,7 @@
+from openerp import fields, models
+class ProductAttachment(models.Model):
+    _inherit = "product.template"
+    attachments = fields.Many2many(
+        comodel_name='ir.attachment',
+        string='Attachments',
+    )
