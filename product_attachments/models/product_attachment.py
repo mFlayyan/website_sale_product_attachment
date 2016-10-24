a6dacbf5ef28e14eaec075e4439f31e60152ab84
gfcapalbo <giovanni@therp.nl>
2016-10-24 12:30:28 +0200

    Assignment and fixes for mariam

--- product_attachments/models/product_attachment.py
+++ product_attachments/models/product_attachment.py
@@ -1,9 +1,11 @@
 from openerp import fields, models
 
 
+
 class ProductAttachment(models.Model):
     _inherit = "product.template"
     attachments = fields.Many2many(
         comodel_name='ir.attachment',
         string='Attachments',
     )
+
