de786a464954831984b427517db3e1f8f3208840
Mariam Flayyan <mflayyan@therp.nl>
2016-11-06 16:32:51 +0200

    bad whitespaces in main.py

--- product_website_restriction/controllers/main.py
+++ product_website_restriction/controllers/main.py
@@ -8,5 +8,6 @@ class WebsiteSale(main.website_sale):
     def _get_search_domain(self, search, category, attrib_values):
         domain = super(WebsiteSale, self)._get_search_domain(
             search=search, category=category, attrib_values=attrib_values)
-        domain += ['|',('websites_ids', '=', False),('websites_ids', 'in', http.request.website.id)]
+        domain += ['|', ('websites_ids', '=', False), (
+            'websites_ids', 'in', http.request.website.id)]
         return domain
