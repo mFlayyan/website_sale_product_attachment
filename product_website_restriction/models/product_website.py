59e05878b7e91798efed188cbf9c4a46137a97e6
Mariam Flayyan <mflayyan@therp.nl>
2016-11-06 12:15:38 +0200

    add spaces for new lines in help string

--- product_website_restriction/models/product_website.py
+++ product_website_restriction/models/product_website.py
@@ -7,6 +7,6 @@ class ProductTemplate(models.Model):
     websites_ids = fields.Many2many(
         comodel_name='website', string='Websites That Have This Product',
         help='These are the websites that will have this product'
-        'please take into consideration that selecting no website is'
-        'considered as all websites are selected',
+        ' please take into consideration that selecting no website is'
+        ' considered as all websites are selected',
     )
