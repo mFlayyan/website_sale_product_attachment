a09ea6dddab4a558d443097779677b993c5fb300
Holger Brunn <hbrunn@therp.nl>
2016-11-08 11:35:30 +0100

    [IMP] wording, move website selection to other tab

--- product_website_restriction/models/product_website.py
+++ product_website_restriction/models/product_website.py
@@ -6,8 +6,8 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     websites_ids = fields.Many2many(
-        comodel_name='website', string='Websites That Have This Product',
-        help='These are the websites that will have this product'
-        ' please take into consideration that selecting no website is'
-        ' considered as all websites are selected',
+        comodel_name='website', string='Websites',
+        help='These are the websites that will have this product. '
+        'Please take into consideration that selecting no website means '
+        'that the product is visible on all available websites',
     )
