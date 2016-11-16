7f63ec61a656113496e3b0c8200668fc652b3e1c
Mariam Flayyan <mflayyan@therp.nl>
2016-11-16 13:43:18 +0200

    chang xmlid

--- product_attachments/models/product_attachment.py
+++ product_attachments/models/product_attachment.py
@@ -2,32 +2,9 @@
 from openerp import fields, models, api
 
 
-class ProductAttachment(models.Model):
+class ProductAttachments(models.Model):
     _inherit = "product.template"
     attachments = fields.Many2many(
         comodel_name='ir.attachment',
         string='Attachments',
     )
-
-    @api.model
-    def create(self, vals):
-        rec = super(ProductAttachment, self).create(vals)
-        base_url = self.env[
-            'ir.config_parameter'].get_param('web.base.url')
-        parent_id = self.env['document.directory'].search([('name', 'ilike', 'shop_attachments_dir')]).id
-        for attachment in self.attachments:
-            attachment.write({'res_id': self.id,
-                              'parent_id': parent_id,})
-        return rec
-
-    @api.multi
-    def write(self, vals, context=None):
-        base_url = self.env['ir.config_parameter'].get_param('web.base.url')
-        result = super(ProductAttachment, self).write(vals, context=context)
-        parent_id = self.env['document.directory'].search([('name', 'ilike', 'shop_attachments_dir')]).id
-        for record in self:
-            for attachment in record.attachments:
-                res = attachment.write({'res_id': record.id,
-                                        'parent_id': parent_id,})
-                result = result and res
-        return result
